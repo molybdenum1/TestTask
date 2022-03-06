00f2507ef6601d3c05d4ae01424d0a8adf5a8389
Denis <63734039+molybdenum1@users.noreply.github.com>
2022-03-06 21:17:40 +0200

    adding form for every button and calc

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let data = `{
         "annualised_return": "8.60",
         "term_remaining": "864000",
         "ltv": "48.80",
-        "amount": "85,754"
+        "amount": "81"
       },
       {
         "id": "5",
@@ -18,7 +18,7 @@ let data = `{
         "annualised_return": "7.10",
         "term_remaining": "1620000",
         "ltv": "48.80",
-        "amount": "85,754"
+        "amount": "22"
       },
       {
         "id": "12",
@@ -28,7 +28,7 @@ let data = `{
         "annualised_return": "4.80",
         "term_remaining": "879000",
         "ltv": "48.80",
-        "amount": "85,754"
+        "amount": "11"
       }
     ]
   }`
@@ -51,22 +51,78 @@ for(let arr of data.loans){
     let div = document.createElement('div');
     div.className = 'ex'
     let h2 = document.createElement("h2")
-    h2.innerText = "Cur Loan" + i
+    h2.innerText = "Cur Loan " + i
     div.appendChild(h2)
     let span = document.createElement("span")
     span.innerText = "Some describe"
     div.appendChild(span)
     let btn = document.createElement("button")
     btn.innerText = "Invest"
+    btn.id = "invs"
     div.appendChild(btn)
+    
+    let divForm = document.createElement("div")
+    div.appendChild(divForm)
+    
+    let form = document.createElement("div")
+    form.id = "form"
+    
+    let hh = document.createElement("h2")
+    hh.innerText = "loan"
+    form.appendChild(hh)
+    let ss = document.createElement("span")
+    ss.id = "title"
+    form.appendChild(ss)
+    let pp = document.createElement("p")
+    pp.id =  "amount"
+    form.appendChild(pp)
+    pp = document.createElement("p")
+    pp.innerText =  "Invest amount"
+    form.appendChild(pp)
+    let input =  document.createElement("input")
+    input.id =   "input"
+    input.type = "number"
+    form.appendChild(input)
+    let sub = document.createElement("button")
+    sub.innerText = "Invest"
+    form.appendChild(sub)
+    
+    divForm.appendChild(form)
+   
+
+
+    btn.addEventListener('click', (e)=>{
+      //console.log(e)
+      console.log(btn.value)
+      let value = btn.value
+      if (form.style.display === "none") {
+        form.style.display = "block";
+      } else {
+        form.style.display = "none";
+      }
+      })
+    
     //console.log(arr)
     for(let key in arr){
-        btn.id = arr['id']
-        console.log(key + " " + arr[key])
-        
-    }
+        btn.value = arr['id']
+        //console.log(key + " " + arr[key])
+        hh.innerText = arr['title']
+        ss.innerText = arr['tranche']
+        pp.innerText = arr['amount']
+        pp.value     = arr['amount']
+        console.log(pp.value)
+    } 
     curLoans.appendChild(div)
     i= i+1;
+
+    sub.addEventListener('click', (e) => {
+      
+      let am = pp.value
+      let kol = input.value
+      console.log(amount - (+am * (+kol)))
+      amount = amount - (+am * (+kol))
+      form.style.display = "none";
+    })
 }
 
 let footer = document.createElement('div')
@@ -74,3 +130,4 @@ let para = document.createElement('p')
 para.innerText = 'total amount availible for investments ' + amount
 footer.appendChild(para)
 curLoans.appendChild(footer)
+
